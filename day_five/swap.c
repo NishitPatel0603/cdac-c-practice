0a07c161ef45c54a244a29945785b6bebb30a639
acabhay30 <85096259+acabhay30@users.noreply.github.com>
2025-09-04 06:54:06 +0530

    diff methods

--- day_five/swap.c
+++ day_five/swap.c
@@ -13,3 +13,32 @@ int main()
 {
     swap(5, 2);
 }
+
+/*#include <stdio.h>
+
+int main()
+{
+    int a = 12;
+    int b = 13;                                                                                     
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("swap is done %d and %d", a,b);
+
+    return 0;
+}
+
+#include <stdio.h>                                            //swapping without extra variables                                       
+
+int main()
+{
+    int a = 12;
+    int b = 13;
+
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    printf("swap is done %d and %d", a, b);
+    return 0;
+}*/
