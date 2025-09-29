5a7c3aa640287dc7ee6fb36b890b57b9b3637b5b
NishitPatel0603 <nishitpatel4535@duck.com>
2025-09-29 17:31:10 +0530

    new

---
+++ mp/first ass/a.c
@@ -0,0 +1,46 @@
+// q1.
+#include <stdio.h>
+
+/*int main()
+{
+    int i=0,n,arr[32] = {0},k = 0;
+
+    printf("enter the number");
+    scanf("%d", &n);
+
+    while (n != 0)
+    {
+        arr[i] = n & 1;
+        n = n >> 1;
+        i++;
+        k++;
+    }
+    for (int j = k - 1; j >= 0 ; j--)
+    {
+        printf("%d ",arr[j]);
+    }
+
+}*/
+
+// q2.
+
+/*int main()
+{
+    int n;
+    printf("enter the number\n");
+    scanf("%d", &n);
+    n = n & 1;
+    if (n)
+    {
+        printf("it is odd");
+    }
+    else
+    {
+        printf("it is even");
+    }
+}*/
+
+int main()
+{
+    
+}
